7b0c8fc0bbda11ef45f6bc05dedff40cca2ecff2
Daniel Perrefort <djperrefort@gmail.com>
2025-01-16 22:13:27 -0500

    Adds form for selecting table from database

---
+++ src/app/common/components/tbl-select-form/tbl-select-form.component.ts
@@ -0,0 +1,78 @@
+import { AsyncPipe } from "@angular/common";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+
+import { Observable } from "rxjs";
+import { map, startWith } from "rxjs/operators";
+
+import { DataService } from "~services/data/data.service";
+
+export interface TblSelectInterface {
+  tableName: string;
+}
+
+@Component({
+  standalone: true,
+  selector: "app-tbl-select-form",
+  templateUrl: "tbl-select-form.component.html",
+  styleUrls: ["tbl-select-form.component.scss"],
+  imports: [
+    AsyncPipe,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
+})
+export class TblSelectFormComponent implements OnInit {
+  @Output() formSubmit = new EventEmitter<TblSelectInterface>();
+
+  protected form!: FormGroup;
+  protected allOptions: string[] = [];
+  protected filteredOptions$!: Observable<string[]>;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.initializeOptions();
+    this.initializeForm();
+    this.initializeAutocomplete();
+  }
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      this.formSubmit.emit(this.form.value);
+    }
+  }
+
+  private initializeOptions(): void {
+    this.dataService.getSchema().subscribe(schema => this.allOptions = Object.keys(schema["tables"]));
+  }
+
+  private initializeForm(): void {
+    const escapedArr = this.allOptions.map(item => item.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1"));
+    const regexPattern = escapedArr.join("|");
+    const regex = new RegExp(`^(${regexPattern})$`);
+
+    this.form = new FormGroup({
+      tableName: new FormControl("", [Validators.required, Validators.pattern(regex)]),
+    });
+  }
+
+  private initializeAutocomplete(): void {
+    this.filteredOptions$ = this.form.get("tableName")!.valueChanges.pipe(
+      startWith(""),
+      map(value => this.filter(value || ""))
+    );
+  }
+
+  private filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.allOptions.filter(option =>
+      option.toLowerCase().includes(filterValue)
+    );
+  }
+}
--- src/app/pages/table-select-page/table-select-page.component.ts
+++ src/app/pages/table-select-page/table-select-page.component.ts
@@ -1,8 +1,30 @@
-import { Component } from "@angular/core";
+import { AsyncPipe } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Observable } from "rxjs";
+
+import { TblSelectFormComponent, TblSelectInterface } from "~components/tbl-select-form/tbl-select-form.component";
+import { DataService } from "~services/data/data.service";
 
 @Component({
   standalone: true,
   selector: "table-select-page",
   templateUrl: "table-select-page.component.html",
+  imports: [
+    AsyncPipe,
+    TblSelectFormComponent,
+  ],
 })
-export class TableSelectPageComponent {}
+export class TableSelectPageComponent implements OnInit {
+  public title$!: Observable<string>;
+
+  constructor(private dataService: DataService, private router: Router) {}
+
+  ngOnInit() {
+    this.title$ = this.dataService.getTitle();
+  }
+
+  handleFormData(formData: TblSelectInterface): void {
+    void this.router.navigate(["app", formData.tableName]);
+  }
+}
