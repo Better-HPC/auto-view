ca02b4c994347f5f0fb4cc22e4937942222bc226
Daniel Perrefort <djperrefort@gmail.com>
2025-01-16 22:54:15 -0500

    High level cleanup pass

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from "@angular/router";
 
+// Layout Components
 import { CenterLayoutComponent } from "~layouts/center-layout/center-layout.component";
 import { MenuBarLayoutComponent } from "~layouts/menu-bar-layout/menu-bar-layout.component";
+
+// Page Components
 import { ErrorPageComponent } from "~pages/error-page/error-page.component";
 import { LoginPageComponent } from "~pages/login-page/login-page.component";
 import { TableDetailPageComponent } from "~pages/table-detail-page/table-detail-page.component";
@@ -11,30 +14,45 @@ export const routes: Routes = [
   {
     path: "",
     redirectTo: "app",
-    pathMatch: "full"
-  }, {
+    pathMatch: "full",
+  },
+  {
     path: "login",
     component: CenterLayoutComponent,
     children: [
-      {path: "", component: LoginPageComponent},
-    ]
-  }, {
+      {
+        path: "",
+        component: LoginPageComponent,
+      },
+    ],
+  },
+  {
     path: "app",
     component: MenuBarLayoutComponent,
     children: [
-      {path: "", component: TableSelectPageComponent},
-      {path: ":tableName", component: TableDetailPageComponent},
-    ]
-  }, {
+      {
+        path: "",
+        component: TableSelectPageComponent,
+      },
+      {
+        path: ":tableName",
+        component: TableDetailPageComponent,
+      },
+    ],
+  },
+  {
     path: "error",
     component: CenterLayoutComponent,
     children: [
-      {path: "", component: ErrorPageComponent},
-    ]
+      {
+        path: "",
+        component: ErrorPageComponent,
+      },
+    ],
   },
   {
     path: "**",
     redirectTo: "/error",
-    pathMatch: "full"
-  }
+    pathMatch: "full",
+  },
 ];
